2c96682e20bcf92b9450745e89731372074e9353
Kristen Stroup <kristenwhitson@gmail.com>
2019-09-14 16:34:28 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+$(".btn").css("background-color", "blue");
+
+
+//create a start button
+//button on click, 10 questions to browser
+//button on click, timer to start
+
+//question to browser
+    //4 multiple choice answer buttons
+    //only allow one button selection per question
+
+//"finish" button at end of questions
+    //create scoreboard
+        //correct
+        //incorrect
+        //unanswered questions
+// if (timer === 0),
+    //score questions answered
+    //un-answered questions === incorrect
+    //final scoreboard
+    //add gif under score
